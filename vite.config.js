2db1983e161be3457b9d87bb2362893f1a5c52ed
KushagraBhadauria <94129983+KushagraBhadauria@users.noreply.github.com>
2024-12-07 17:56:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true
-  },
+  // build: {
+  //   sourcemap: true
+  // },
    base: '/iPhone-15-Pro/',
 
 })
